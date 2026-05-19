906d2611ced905b4e73e3ecc32258571343c08c1
isendev <isen.osman252@gmail.com>
2026-05-19 18:24:44 +0200

    Fix: update base path to isenverse for UI to load

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Isen-Osman.github.io',
-  base: '/my-portfolio', // Repository name
+  base: '/isenverse', // Exact name of your GitHub repository
   outDir: './dist',
 });
